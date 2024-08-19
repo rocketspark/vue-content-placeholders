c76ad32de32736150da0bdd582076909ad0c4fd0
Lee Reichardt <lee.reichardt@rocketspark.com>
2024-08-20 08:10:54 +1200

    Added type defs

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as ContentPlaceholders } from './ContentPlaceholders.vue';
+export { default as ContentPlaceholdersHeading } from './ContentPlaceholdersHeading.vue';
+export { default as ContentPlaceholdersImg } from './ContentPlaceholdersImg.vue';
+export { default as ContentPlaceholdersText } from './ContentPlaceholdersText.vue';
